b5c67cc91859c84be8209e65133f5efbc38d0a87
WPExplorer <wpexplorer@gmail.com>
2020-10-17 12:46:27 -0700

    Cleaned up functions.php commenting and added ABSPATH check.

--- functions.php
+++ functions.php
@@ -2,23 +2,11 @@
 /**
  * Functions and definitions.
  *
- * Sets up the theme and provides some helper functions
- *
- * When using a child theme (see http://codex.wordpress.org/Theme_Development
- * and http://codex.wordpress.org/Child_Themes), you can override certain
- * functions (those wrapped in a function_exists() call) by defining them first
- * in your child theme's functions.php file. The child theme's functions.php
- * file is included before the parent theme's file, so the child theme
- * functions would be used.
- *
- *
- * For more information on hooks, actions, and filters,
- * see http://codex.wordpress.org/Plugin_API
- *
  * @package WordPress
  * @subpackage Office WordPress Theme
  */
 
+defined( 'ABSPATH' ) || exit;
 
 /*--------------------------------------*/
 /* Define Constants
